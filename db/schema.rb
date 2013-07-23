49096bee3c17fe7ffc0baf932f210d28eac2208e
ctyeh <ctyeh@post.harvard.edu>
2013-07-23 11:12:15 -0700

    Change Micropost Content to Text

--- db/schema.rb
+++ db/schema.rb
@@ -47,13 +47,11 @@ ActiveRecord::Schema.define(:version => 20130719010836) do
   add_index "disagreements", ["disagreer_id"], :name => "index_disagreements_on_disagreer_id"
 
   create_table "microposts", :force => true do |t|
-    t.string   "content"
+    t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
-
-  add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
 
   create_table "relationships", :force => true do |t|
     t.integer  "follower_id"
