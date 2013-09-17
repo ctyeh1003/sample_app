e796fcfee5b12349be69d82b02cd6f9aa86fc1a9
ctyeh <ctyeh@post.harvard.edu>
2013-09-17 09:11:24 -0700

    changed title to rereason.com

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Agree or Disagree"
+    base_title = "Rereason.com"
     if page_title.empty?
       base_title
     else
